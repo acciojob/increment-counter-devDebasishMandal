d959273f5326313d77761c11bf5b2b8658cbc142
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-03-04 01:14:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+
+let x=document.getElementById("counter");
+let btn=document.getElementById("incrementBtn");
+let count=0;
+btn.addEventListener("click",()=>{
+	count++;
+	x.innerHTML=count;
+})
